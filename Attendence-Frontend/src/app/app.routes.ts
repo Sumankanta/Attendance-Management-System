5584ff5550db1ba795604c541d5ae073b7889200
sumankanta <sumankpadhan@gmail.com>
2026-02-14 21:05:58 +0530

    feat(frontend): implement role-based sidebars
    - Added sidebar components for Admin, Manager, and Employee
    - Made sidebars dynamic based on authenticated user role
    - Integrated role checks with Angular services
    - Ensured consistent navigation experience across roles

--- Attendence-Frontend/src/app/app.routes.ts
+++ Attendence-Frontend/src/app/app.routes.ts
@@ -2,5 +2,8 @@ import { Routes } from '@angular/router';
 import Login from './basic/login/login';
 
 export const routes: Routes = [
-  {path:'', component: Login}
+  {path:'', component: Login},
+  {path:'admin', loadChildren: () => import('./admin/admin-module').then(m => m.AdminModule)},
 ];
+
+// ng g m admin --routing
--- Attendence-Frontend/src/app/app.ts
+++ Attendence-Frontend/src/app/app.ts
@@ -1,12 +1,32 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared-module';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { UserStorage } from './basic/basic-services/user-storage';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SharedModule, NzBreadCrumbModule, NzIconModule, NzMenuModule, NzLayoutModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Attendence-Frontend');
+  title = 'Attendence-Frontend';
+
+  isEmploeeLoggedIn:boolean = UserStorage.isEmployeeLoggedIn();
+  isManagerLoggedIn:boolean = UserStorage.isManagerLoggedIn();
+  isAdminLoggedIn:boolean = UserStorage.isAdminLoggedIn();
+
+  constructor(private router: Router){}
+
+  ngOnInit(){
+    this.router.events.subscribe((events) => {
+      this.isEmploeeLoggedIn = UserStorage.isEmployeeLoggedIn();
+      this.isManagerLoggedIn = UserStorage.isManagerLoggedIn();
+      this.isAdminLoggedIn = UserStorage.isAdminLoggedIn();
+    });
+  }
 }
